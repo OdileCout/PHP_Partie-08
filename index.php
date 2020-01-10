9864a822ba41acf2a508cc8227a6eac8f415f9a4
Votre Nom <odilecoutiez@gmail.com>
2020-01-10 16:27:43 +0100

    Les fichier parent pour le host

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+        <title>Partie-VIII</title>
+    </head>
+    <body>
+        <div class="">
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-1/index.php">Exercice-1</a></button>
+                </div>
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-2/index.php">Exercice-2</a></button>
+                </div>
+            </div>
+            <div class="offset-4 col-md-4 text-center middleDiv">
+                <button type="button"><a href="exo-3/index.php">Exercice-3</a></button>
+            </div>
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-4/index.php">Exercice-4</a></button>
+                </div>
+                <div class="col-md-6 text-center">
+                    <button type="button"><a href="exo-5/index.php">Exercice-5</a></button>
+                </div> 
+            </div>
+        </div>
+    </body>
+</html>
